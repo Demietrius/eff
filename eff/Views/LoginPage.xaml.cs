fade7af2e075669cdfb7a0ac152ffe0bca6a61cf
Demietrius Huff <32649066+Demietrius@users.noreply.github.com>
2019-09-28 22:43:06 -0500

    added functionality to login.  having problems with database connection

--- eff/Views/LoginPage.xaml.cs
+++ eff/Views/LoginPage.xaml.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using eff.Models;
+
 
 namespace eff.Views
 {
+    
     public partial class LoginPage : ContentPage
     {
+        Database_connection database_Connection = new Database_connection();
+
         public LoginPage()
         {
             InitializeComponent();
         }
+        private async void Login(object sender, EventArgs e)
+        {
+            string message = database_Connection.TestDatabaseConnection();
+
+        }
+      
     }
 }
