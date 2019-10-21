6b920fcffd8da306772f2ce506254b16c891ea1d
Demietrius Huff <32649066+Demietrius@users.noreply.github.com>
2019-10-21 13:32:29 -0500

    added user model and usermanger database connection

---
+++ eff/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eff.Models
+{
+    class User
+    {
+           
+        [JsonProperty(PropertyName = "userId")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "Username")]
+        public string Username { get; set; }
+
+        [JsonProperty(PropertyName = "password")]
+        public String Password { get; set; }
+
+        [JsonProperty(PropertyName = "filter")]
+        public int Filters { get; set; }
+
+        [JsonProperty(PropertyName = "date")]
+        public DateTime Date { get; set; }
+
+        [JsonProperty(PropertyName = "number of games played")]
+        public int GamesPlayed { get; set; }
+
+        [JsonProperty(PropertyName = "groupId")]
+        public int GroupId { get; set; }
+    }
+}
---
+++ eff/ViewModels/UserManger.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eff.ViewModels
+{
+    class UserManger
+    {
+        static UserManger defaultInstance = new UserManger();
+
+        const string accountURL = @"https://effdatabase.documents.azure.com:443/";
+        const string accountKey = @"0LhL8FvWxysH8SLdx0GUlD2OLlghxX1jJcYAPVceYBgi32ocmNwvbQJHJlthaSy5eBKEH6uXIwTyrEEFChDWJA==";
+        const string databaseId = @"user";
+        const string collectionId = @"clients";
+
+        private Uri collectionLink = UriFactory.CreateDocumentCollectionUri(databaseId, collectionId);
+
+        private DocumentClient client;
+
+        private UserManger()
+        {
+            client = new DocumentClient(new System.Uri(accountURL), accountKey);
+        }
+
+        public static UserManger DefaultManager
+        {
+            get
+            {
+                return defaultInstance;
+            }
+            private set
+            {
+                defaultInstance = value;
+            }
+        }
+
+        public List<User> users { get; private set; }
+
+        public async Task<List<User>> GetUserById(int UserId)
+        {
+            try
+            {
+                // The query excludes completed TodoItems
+                var query = client.CreateDocumentQuery<User>(collectionLink, new FeedOptions { MaxItemCount = -1 })
+                      .Where(user => user.Id.Equals(UserId))
+                      .AsDocumentQuery();
+
+                users = new List<User>();
+                while (query.HasMoreResults)
+                {
+                    users.AddRange(await query.ExecuteNextAsync<User>());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(@"ERROR {0}", e.Message);
+                return null;
+            }
+            return users;
+        }
+
+        private bool IsExsitingUser(User NewUser)
+        {
+            try
+            {
+                var query = client.CreateDocumentQuery<User>(collectionLink, new FeedOptions { MaxItemCount = -1 })
+                       .Where(user => user.Username.Equals(NewUser.Username))
+                       .AsDocumentQuery();
+                if (query.HasMoreResults)
+                    return false;
+                else
+                    return true;
+
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(@"Error{0}", e.Message);
+            }
+
+            return true;
+        }
+
+
+        public async Task<User> InsertUser(User user)
+        {
+            if (!IsExsitingUser(user))
+            {
+                return null;
+            }
+
+            try
+            {
+
+                var result = await client.CreateDocumentAsync(collectionLink, user);
+                user.Id = result.Resource.Id;
+                users.Add(user);
+
+
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(@"ERROR {0}", e.Message);
+            }
+            return user;
+        }
+
+        /*public async Task CompleteItemAsync(User user)
+        {
+            try
+            {
+                item.Complete = true;
+                await client.ReplaceDocumentAsync(UriFactory.CreateDocumentUri(databaseId, collectionId, item.Id), item);
+
+                Items.Remove(item);
+
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(@"ERROR {0}", e.Message);
+            }*/
+
+    }
+}
