46278df88b6c0810532c47861a8d89b58bc3bc0e
Josh <jms49800@ucmo.edu>
2019-10-25 13:20:09 -0500

    Swapped to Yelp api

---
+++ eff/Models/Place.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eff.Models
+{
+    public class Place
+    {
+        public string name {get; set;}
+        public string image_url {get; set;}
+        public string is_closed {get; set;}
+        public string url {get; set;}
+        public string rating {get; set;}
+        public string coordinates {get; set;}
+        public string price {get; set;}
+        public string display_address {get; set;}
+        public string display_phone {get; set;}
+        public string distance {get; set;}
+    }
+}
--- eff/Views/NearbyRestaurants.xaml.cs
+++ eff/Views/NearbyRestaurants.xaml.cs
@@ -8,6 +8,8 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Plugin.Geolocator;
+using System.Collections.ObjectModel;
+using eff.Models;
 
 namespace eff.Views
 {
@@ -17,19 +19,18 @@ namespace eff.Views
         public NearbyRestaurants()
         {
             InitializeComponent();
+            PlacesView.ItemsSource = places;
+
+            // ObservableCollection allows items to be added after ItemsSource
+            // is set and the UI will react to changes
+            places.Add(new Place { name = "Rob Finnerty" });
+            places.Add(new Place { name = "Bill Wrestler" });
+            places.Add(new Place { name = "Dr. Geri-Beth Hooper" });
+            places.Add(new Place { name = "Dr. Keith Joyce-Purdy" });
+            places.Add(new Place { name = "Sheri Spruce" });
+            places.Add(new Place { name = "Burt Indybrick" });
         }
 
-        private void RequestCompleted(IAsyncResult result)
-        {
-            var request = (HttpWebRequest)result.AsyncState;
-            var response = (HttpWebResponse)request.EndGetResponse(result);
-            using (var stream = response.GetResponseStream())
-            {
-                var r = new StreamReader(stream);
-                var resp = r.ReadToEnd();
-            }
-
-        }
 
         protected async void RequestPlaces_Clicked(object sender, EventArgs e)
         {
@@ -40,23 +41,49 @@ namespace eff.Views
             //String longitude = position.Longitude.ToString();
             String latitude = "39.032329";
             String longitude = "-94.348950";
+            String categories = "food";
+            int radius = 1610;
 
-            //easy-to-copy example displaying the returned json
-            //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=39.032329,-94.348950&radius=1610&sensor=true&type=restaurant&key=AIzaSyBaWchHhb7uEWp81QYBVGLf86WlKx1p1OI
-            //radius is measured in meters, max is 50,000
+            //documentation https://www.yelp.com/developers/documentation/v3/business_search
+            //radius is measured in meters, max is 40,000(~25 miles)
             //1610 meters = 1 mile
-            string placesSearchString = "https://maps.googleapis.com/maps/api/place/nearbysearch/" +
-                "json?location=" + latitude + "," + longitude +
-                "&radius=1610&sensor=true" +
-                "&type=restaurant" +
-                "&key=AIzaSyBaWchHhb7uEWp81QYBVGLf86WlKx1p1OI";
+            //Yelp api key : FxutAF1Xi3y_LjzpSKaV9aijfwKwcLLOs8APnHCcPu8FhZyKHvxPvS9_Fe_hx5jmcuPWr1nvBg6LJGiaiBp4YFi-uWVBAo-mqgNKD22bP0EdhsCdmOrveY6XX1myXXYx
+            string placesSearchString = "https://api.yelp.com/v3/businesses/search" +
+                "?latitude=" + latitude +
+                "&longitude=" + longitude +
+                "&categories=" + categories +
+                "&radius=" + radius;
 
             HttpWebRequest webRequest = WebRequest.Create(placesSearchString) as HttpWebRequest;
             webRequest.Timeout = 20000;
+            webRequest.Headers.Add("Authorization", "Bearer FxutAF1Xi3y_LjzpSKaV9aijfwKwcLLOs8APnHCcPu8FhZyKHvxPvS9_Fe_hx5jmcuPWr1nvBg6LJGiaiBp4YFi-uWVBAo-mqgNKD22bP0EdhsCdmOrveY6XX1myXXYx");
             webRequest.Method = "GET";
 
-            webRequest.BeginGetResponse(new AsyncCallback(RequestCompleted), webRequest);
+            HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse();
+
+            var stream = new StreamReader(webResponse.GetResponseStream(), Encoding.UTF8);
+            var content = stream.ReadToEnd();
+            Console.Write(content);
+            System.Diagnostics.Debug.WriteLine(content);
+            System.Diagnostics.Debug.WriteLine("FINDME");
         }
+
+        ObservableCollection<Place> places = new ObservableCollection<Place>();
+        public ObservableCollection<Place> Places { get { return places; } }
+
+/*        public NearbyRestaurants()
+        {
+            PlacesView.ItemsSource = places;
+
+            // ObservableCollection allows items to be added after ItemsSource
+            // is set and the UI will react to changes
+            places.Add(new Place { name = "Rob Finnerty" });
+            places.Add(new Place { name = "Bill Wrestler" });
+            places.Add(new Place { name = "Dr. Geri-Beth Hooper" });
+            places.Add(new Place { name = "Dr. Keith Joyce-Purdy" });
+            places.Add(new Place { name = "Sheri Spruce" });
+            places.Add(new Place { name = "Burt Indybrick" });
+        }*/
     }
 
 }
