dc9ddf719b7cd540873b937271bf8763f3f05910
Jacob <jda36140@ucmo.edu>
2019-11-14 21:58:01 -0600

    added nearby

--- eff/Views/UserHome.xaml.cs
+++ eff/Views/UserHome.xaml.cs
@@ -14,10 +14,10 @@ namespace eff.Views
             InitializeComponent();
             Application.Current.Properties["IsLoggedIn"] = Boolean.TrueString;
             ////////////////////////////////testing
-            var user = new User() { Username = "testing" };
+            //var user = new User() { Username = "testing" };
 
-            //////////////////////////////////////
-            Lbl_user.SetBinding(Label.TextProperty,new Binding(user.Username));
+            ////////////////////////////////////////
+            //Lbl_user.SetBinding(Label.TextProperty,new Binding(user.Username));
 
         }
 
@@ -34,6 +34,11 @@ namespace eff.Views
         {
             await Navigation.PushAsync(new CreateLobby());
            
+        }
+
+        private async void OnNearbyClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Nearby());
         }
 
         private async void Logout(object sender, EventArgs e)
