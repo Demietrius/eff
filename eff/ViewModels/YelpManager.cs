485825031c725c2af4cf795bbb1a76f77d4c37b7
Josh <jms49800@ucmo.edu>
2019-11-18 06:09:04 -0600

    Moved Yelp API logic to YelpManager.cs

--- eff/ViewModels/YelpManager.cs
+++ eff/ViewModels/YelpManager.cs
@@ -11,6 +11,7 @@ using Plugin.Geolocator;
 using System.Collections.ObjectModel;
 using eff.Models;
 using Newtonsoft.Json.Linq;
+using Xamarin.Essentials;
 
 namespace eff.ViewModels
 {
@@ -18,22 +19,35 @@ namespace eff.ViewModels
 
     class YelpManager
     {
-
+        static YelpManager defaultInstance = new YelpManager();
         public ObservableCollection<Place> Places { get; } = new ObservableCollection<Place>();
-  
+        public int LikeCount { get; set; }
+        public int NumberOfPlaces { get; set; }
 
-        public async void RequestPlaces_Clicked()
+
+        public static YelpManager DefaultManager
         {
-            var locator = CrossGeolocator.Current;
-            locator.DesiredAccuracy = 50;
-            var position = await locator.GetPositionAsync(TimeSpan.FromSeconds(1000), null, true);
-            //String latitude =  position.Latitude.ToString();
-            //String longitude = position.Longitude.ToString();
-            String latitude = "39.032329";
-            String longitude = "-94.348950";
-            String categories = "food";
-            int radius = 1610;
+            get
+            {
+                return defaultInstance;
+            }
+            private set
+            {
+                defaultInstance = value;
+            }
+        }
 
+        public async Task<Tuple<string, string>> GetUserLocationAsync()
+        {
+            var request = new GeolocationRequest(GeolocationAccuracy.High);
+            var location = await Geolocation.GetLocationAsync(request);
+            return new Tuple<string, string>(location.Latitude.ToString(), location.Longitude.ToString());
+        }
+
+        public string GenerateYelpSearchString(String latitude, String longitude)
+        {
+            String categories = "Restaurant";
+            int radius = 1610;
             //documentation https://www.yelp.com/developers/documentation/v3/business_search
             //radius is measured in meters, max is 40,000(~25 miles)
             //1610 meters = 1 mile
@@ -44,25 +58,44 @@ namespace eff.ViewModels
                 "&categories=" + categories +
                 "&radius=" + radius;
 
-            HttpWebRequest webRequest = WebRequest.Create(placesSearchString) as HttpWebRequest;
+            return placesSearchString;
+        }
+
+        public JObject YelpWebRequest(String searchString)
+        {
+            HttpWebRequest webRequest = WebRequest.Create(searchString) as HttpWebRequest;
             webRequest.Timeout = 20000;
             webRequest.Headers.Add("Authorization", "Bearer FxutAF1Xi3y_LjzpSKaV9aijfwKwcLLOs8APnHCcPu8FhZyKHvxPvS9_Fe_hx5jmcuPWr1nvBg6LJGiaiBp4YFi-uWVBAo-mqgNKD22bP0EdhsCdmOrveY6XX1myXXYx");
             webRequest.Method = "GET";
-
             HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse();
-
             var stream = new StreamReader(webResponse.GetResponseStream(), Encoding.UTF8);
             var content = stream.ReadToEnd();
 
-            JObject joResponse = JObject.Parse(content);
-            JArray array = (JArray)joResponse["businesses"];
-            string businesses = array[0].ToString();
+            return JObject.Parse(content);
+        }
 
+        public void ParseJObjectResponse(JObject joResponse)
+        {
             foreach (JToken bus in joResponse.SelectToken("businesses"))
             {
                 string b = (string)bus.SelectToken("name");
-                Places.Add(new Place { name = b });
+                string c = (string)bus.SelectToken("image_url");
+                string d = (string)bus.SelectToken("rating");
+                Places.Add(new Place { name = b, image_url = c, rating = d, Isliked = false });
             }
+            NumberOfPlaces = Places.Count;
+        }
+
+        
+
+        public void error() { }
+
+        public bool CheckLikes()
+        {
+            if (LikeCount < (int)(0.30 * NumberOfPlaces))
+                return true;
+            else
+                return false;
         }
 
     }
--- eff/Views/GetPlaces.xaml.cs
+++ eff/Views/GetPlaces.xaml.cs
@@ -12,85 +12,47 @@ using System.Collections.ObjectModel;
 using eff.Models;
 using Newtonsoft.Json.Linq;
 using Xamarin.Essentials;
+using eff.ViewModels;
 
 namespace eff.Views
 {
    
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class GetPlaces : ContentPage
-	{        
-		
-		public ObservableCollection<Place> Places { get; } = new ObservableCollection<Place>();
-		public int LikeCount { get; set; }
-		public int NumberOfPlaces { get; set; }
-		public GetPlaces(User user)
+	{
+        YelpManager yelpManager;
+        public GetPlaces()
 		{
 			InitializeComponent();
-			PlacesView.ItemsSource = Places;
-			LikeCount = 0;
-			NumberOfPlaces = 0;
-		}
-		protected async void RequestPlaces_Clicked(object sender, EventArgs e)
-		{
-			var request = new GeolocationRequest(GeolocationAccuracy.High);
-			var location = await Geolocation.GetLocationAsync(request);
-			String latitude = location.Latitude.ToString();
-			String longitude = location.Longitude.ToString();
-			String categories = "Restaurant";
-			int radius = 1610;
-			//documentation https://www.yelp.com/developers/documentation/v3/business_search
-			//radius is measured in meters, max is 40,000(~25 miles)
-			//1610 meters = 1 mile
-			//Yelp api key : FxutAF1Xi3y_LjzpSKaV9aijfwKwcLLOs8APnHCcPu8FhZyKHvxPvS9_Fe_hx5jmcuPWr1nvBg6LJGiaiBp4YFi-uWVBAo-mqgNKD22bP0EdhsCdmOrveY6XX1myXXYx
-			string placesSearchString = "https://api.yelp.com/v3/businesses/search" +
-				"?latitude=" + latitude +
-				"&longitude=" + longitude +
-				"&categories=" + categories +
-				"&radius=" + radius;
-			Console.WriteLine(placesSearchString + "  FINDME");
-			HttpWebRequest webRequest = WebRequest.Create(placesSearchString) as HttpWebRequest;
-			webRequest.Timeout = 20000;
-			webRequest.Headers.Add("Authorization", "Bearer FxutAF1Xi3y_LjzpSKaV9aijfwKwcLLOs8APnHCcPu8FhZyKHvxPvS9_Fe_hx5jmcuPWr1nvBg6LJGiaiBp4YFi-uWVBAo-mqgNKD22bP0EdhsCdmOrveY6XX1myXXYx");
-			webRequest.Method = "GET";
-			HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse();
-			var stream = new StreamReader(webResponse.GetResponseStream(), Encoding.UTF8);
-			var content = stream.ReadToEnd();
-			JObject joResponse = JObject.Parse(content);
-			JArray array = (JArray)joResponse["businesses"];
-			string businesses = array[0].ToString();
-			  
-			
-			foreach (JToken bus in joResponse.SelectToken("businesses"))
-			{
-				string b = (string)bus.SelectToken("name");
-				string c = (string)bus.SelectToken("image_url");
-				string d = (string)bus.SelectToken("rating");
-				Places.Add(new Place { name = b, image_url = c, rating = d, Isliked = false});
-			}
-            NumberOfPlaces = Places.Count;
-		   
-		}
+            yelpManager = YelpManager.DefaultManager;
+            PlacesView.ItemsSource = yelpManager.Places;
+            yelpManager.LikeCount = 0;
+            yelpManager.NumberOfPlaces = 0;
+        }
 
-		protected void LabelClicked(object sender, SelectionChangedEventArgs e)
-		{
-			var test = e.CurrentSelection;
-			
-			if (!CheckLikes())
-				error();
+        protected void RequestPlaces_Clicked(object sender, EventArgs e)
+        {
+            var location = yelpManager.GetUserLocationAsync();
+            String latitude = location.Result.Item1;
+            String longitude = location.Result.Item2;
 
-			LikeCount++;
-			
-		} 
+            var searchString = yelpManager.GenerateYelpSearchString(latitude, longitude);
 
-		private void error() { }
+            JObject joResponse = yelpManager.YelpWebRequest(searchString);
 
-		private bool CheckLikes()
-		{
-			if (LikeCount < (int)(0.30 * NumberOfPlaces))
-				return true;
-			else
-				return false;
-		}
+            yelpManager.ParseJObjectResponse(joResponse);
 
-	}
+        }
+        protected void LabelClicked(object sender, SelectionChangedEventArgs e)
+        {
+            var test = e.CurrentSelection;
+
+            if (!yelpManager.CheckLikes())
+                yelpManager.error();
+
+            yelpManager.LikeCount++;
+
+        }
+
+    }
 }
